449d6f6a5141ecc34430d077a9767ee3fd3d1f68
omkar <omkarsgothankar@gmail.com>
2022-11-13 17:19:51 +0530

    gujarat padding

--- states/gujarat.js
+++ states/gujarat.js
@@ -1,11 +1,13 @@
-const requestPauser = require('../utils/requestPauser');
+const requestPauser = require("../utils/requestPauser");
 
-async function captchaHandling(page, link, time_now){
+async function captchaHandling(page, link, time_now) {
   await page.goto(link);
 }
 
 async function gujarat(dist, ac, pn) {
-  const URL = `https://erms.gujarat.gov.in/ceo-gujarat/DRAFT2022/0${ac < 10 ? ('0'+ac) : ac}/S06A${ac}P${pn}.pdf`;
+  const URL = `https://erms.gujarat.gov.in/ceo-gujarat/DRAFT2022/${ac
+    .toString()
+    .padStart(3, "0")}/S06A${ac}P${pn}.pdf`;
 
   await requestPauser(URL, captchaHandling);
 }
