474d51b5e0b14b5f86dad8820261f7f0289982e3
omkar <omkarsgothankar@gmail.com>
2022-11-13 16:07:05 +0530

    westBengal

---
+++ states/westBengal.js
@@ -0,0 +1,17 @@
+const requestPauser = require("../utils/requestPauser");
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link);
+}
+
+async function westBengal(dist, ac, pn) {
+  // TODO: Convert 09 to 9
+  // dist id 09-9
+  const URL = `https://ceowestbengal.nic.in/DraftRoll?DCID=${dist}%20&ACID=${ac}&PSID=${pn}#toolbar=0&navpanes=0`;
+
+  await requestPauser(URL, captchaHandling);
+  // Do somehting with pdf [take epic id too above]
+}
+
+//module.exports= westBengal
+westBengal(2, 14, 172);
