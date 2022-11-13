84bc361885ac3bb99272102d38abf75eb0e3e3c4
omkar <omkarsgothankar@gmail.com>
2022-11-13 17:05:56 +0530

    goa

---
+++ states/goa.js
@@ -0,0 +1,19 @@
+const requestPauser = require("../utils/requestPauser");
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link);
+}
+
+async function goa(dist, ac, pn) {
+  // TODO: Convert 09 to 9
+  // dist name can be null
+
+  // TODO: for loop for looping over pdf pages stops early in case of ac=1 and pn=1
+  const URL = `https://ceogoa.nic.in/PDF/EROLL/MOTHERROLL/2021/${ac}/S05A${ac}P${pn}.pdf`;
+
+  await requestPauser(URL, captchaHandling);
+  // Do somehting with pdf [take epic id too above]
+}
+
+//module.exports= goa
+goa("", 1, 1);
