d22a4d06e8cfd7b7595e926dfdfe09f6964bc934
code-death <poorvanshkavta938@gmial.com>
2022-11-13 17:47:11 +0530

    Daman Completed

---
+++ states/daman.js
@@ -0,0 +1,16 @@
+const requestPauser = require('../utils/requestPauser')
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link)
+}
+
+async function daman(dist, ac, pn) {
+  const URL = `https://ceodaman.nic.in/ElectoralRoll/PhotoER2022_F/pdf/English/A00100${
+    pn < 10 ? pn.toString().padStart(2, '0') : pn
+  }.PDF`
+
+  await requestPauser(URL, captchaHandling)
+}
+
+module.exports = daman
+daman(null, null, 10)
