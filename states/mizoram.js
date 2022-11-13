52aff5c753d42bf8df5a6f5b351e50785b6679ee
omkar <omkarsgothankar@gmail.com>
2022-11-13 16:29:08 +0530

    mizoram(check input)

---
+++ states/mizoram.js
@@ -0,0 +1,18 @@
+const requestPauser = require("../utils/requestPauser");
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link);
+}
+
+async function mizoram(dist, ac, pn) {
+  // TODO: Convert 09 to 9
+  // dist is null
+  // TODO: HERE AC NAME IS ALSO REQUIRED
+  const URL = `https://ceo.mizoram.gov.in/ERollReportWithoutPhoto/S16/${ac}-PALAK/S16A${ac}P${pn}.pdf`;
+
+  await requestPauser(URL, captchaHandling);
+  // Do somehting with pdf [take epic id too above]
+}
+
+//module.exports= mizoram
+mizoram("", 40, 44);
