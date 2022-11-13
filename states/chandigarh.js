655181c78582009c4ce241a5698a18d2134791c3
code-death <poorvanshkavta938@gmial.com>
2022-11-13 17:13:36 +0530

    Chandigarh2

---
+++ states/chandigarh.js
@@ -0,0 +1,19 @@
+const requestPauser = require('../utils/requestPauser')
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link)
+}
+
+async function chandigarh(dist, ac, pn) {
+  if (pn < 10) {
+    pn = '00' + pn
+  } else if (pn < 100) {
+    pn = '0' + pn
+  }
+  const URL = `https://ceochandigarh.gov.in/sites/default/files/polling_files/w0010${pn}.pdf`
+
+  await requestPauser(URL, captchaHandling)
+}
+
+module.exports = chandigarh
+// chandigarh(null, null, 9)
