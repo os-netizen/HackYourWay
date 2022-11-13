9deecb7d985259d80fd78c621e5024e3278f307d
omkar <omkarsgothankar@gmail.com>
2022-11-13 16:15:36 +0530

    nagaland

---
+++ states/nagaland.js
@@ -0,0 +1,17 @@
+const requestPauser = require("../utils/requestPauser");
+
+async function captchaHandling(page, link, time_now) {
+  await page.goto(link);
+}
+
+async function nagaland(dist, ac, pn) {
+  // TODO: Convert 09 to 9
+  // dist is null
+  const URL = `https://ceo.nagaland.gov.in/Downloads/DraftRoll2023/${ac}/S17A${ac}P${pn}.pdf`;
+
+  await requestPauser(URL, captchaHandling);
+  // Do somehting with pdf [take epic id too above]
+}
+
+//module.exports= nagaland
+nagaland("", 50, 2);
