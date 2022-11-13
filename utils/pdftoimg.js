623734139fd7fef8699e786fcd04c4e866d85ade
code-death <poorvanshkavta938@gmial.com>
2022-11-13 16:35:58 +0530

    Chandigarh complete

--- utils/pdftoimg.js
+++ utils/pdftoimg.js
@@ -1,18 +1,22 @@
-const pdf2img = require("pdf-img-convert");
-const fs=require('fs');
+const pdf2img = require('pdf-img-convert')
+const fs = require('fs')
 
 async function pdfToImg(pdfPath) {
-  pdfArray = await pdf2img.convert(pdfPath, {width: 1200, height: 1700});
+  pdfArray = await pdf2img.convert(pdfPath, { width: 1200, height: 1700 })
   // TODO: check number of iterations
-  for (i = 2; i < pdfArray.length-1; i++) {
-    fs.writeFile(`images/roll/${pdfPath}` + i + `.png`, pdfArray[i], function (error) {
-      if (error) {
-        console.error("Error: " + error);
+  for (i = 2; i < pdfArray.length - 1; i++) {
+    fs.writeFile(
+      `../images/roll/${pdfPath}` + i + `.png`,
+      pdfArray[i],
+      function (error) {
+        if (error) {
+          console.error('Error: ' + error)
+        }
       }
-    });
+    )
   }
 
   // TODO: delete images after use
-};
+}
 
-module.exports = pdfToImg;
+module.exports = pdfToImg
