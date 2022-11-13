7a2188afd6a3e600952d92bb633f92fb6d9a33d9
omkar <omkarsgothankar@gmail.com>
2022-11-13 21:25:33 +0530

    index js

---
+++ states/index.js
@@ -0,0 +1,37 @@
+const andhraFunc=require('./andhra')
+const assamFunc=require('./assam')
+const chandigarhFunc=require('./chandigarh')
+const chattisgarhFunc=require('./chattisgarh')
+const damanFunc=require('./daman')
+const delhiFunc=require('./delhi')
+const goaFunc=require('./goa')
+const gujaratFunc=require('./gujarat')
+const karnatakaFunc=require('./gujarat')
+const meghalayaFunc=require('./meghalaya')
+const mizoramFunc=require('./mizoram')
+const nagalandFunc=require('./nagaland')
+const odishaFunc=require('./odisha')
+const rajasthan=require('./rajasthan')
+const sikkimFunc=require('./sikkim')
+const telanganaFunc=require('./telangana')
+const westBengalFunc=require('./westBengal')
+
+module.exports = {
+  andhraFunc,
+  assamFunc,
+  chandigarhFunc,
+  chattisgarhFunc,
+  damanFunc,
+  goaFunc,
+  delhiFunc,
+  gujaratFunc,
+  karnatakaFunc,
+  meghalayaFunc,
+  mizoramFunc,
+  nagalandFunc,
+  odishaFunc,
+  rajasthan,
+  sikkimFunc,
+  westBengalFunc,
+  telanganaFunc
+};
